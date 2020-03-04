efb5b99b9c018b4785da2f377c20d4d157dc9584
yehuo <1505191091@qq.com>
2020-03-04 21:06:35 +0800

    新建项目

---
+++ src/main/java/com/mcy/zhihuspider/ZhihuSpiderApplication.java
@@ -0,0 +1,13 @@
+package com.mcy.zhihuspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuSpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhihuSpiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcy/zhihuspider/ZhihuSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcy.zhihuspider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhihuSpiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
