919cf5177f7cfd7bb6c2fcadb96544b140315bfe
yehuo <1505191091@qq.com>
2020-03-05 21:35:21 +0800

    MQ改为Kafka

--- src/main/java/com/mcy/zhihuspider/InfoBoomSpiderApplication.java
+++ src/main/java/com/mcy/zhihuspider/InfoBoomSpiderApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhihuSpiderApplication {
+public class InfoBoomSpiderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZhihuSpiderApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InfoBoomSpiderApplication.class, args);
+    }
 
 
 }
