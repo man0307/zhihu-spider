62e6f12273e0170f24dee0a825dc8b8aaa76d1a0
yehuo <1505191091@qq.com>
2020-03-05 17:11:53 +0800

    接入Swagger

--- src/main/java/com/mcy/zhihuspider/config/SwaggerConfiguration.java
+++ src/main/java/com/mcy/zhihuspider/config/SwaggerConfiguration.java
@@ -1,6 +1,14 @@
 package com.mcy.zhihuspider.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author chaoyang.man
@@ -8,6 +16,25 @@ import org.springframework.context.annotation.Configuration;
  * @date 2020/3/5 9:40 上午
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
-    //todo 接入Swagger
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mcy.zhihuspider.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("热点信息聚合站")
+                        .description("聚合各类网站的热点信息，营造热点讨论社区")
+                        .version("1.0")
+                        .contact(new Contact("chaoyang.mcy", "blog.csdn.net", "1505191091@qq.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")
+                        .build());
+    }
+
 }
---
+++ src/main/java/com/mcy/zhihuspider/domain/ResultModel.java
@@ -0,0 +1,78 @@
+package com.mcy.zhihuspider.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chaoyang.man
+ * @version 1.0
+ * @date 2020/3/4 7:58 下午
+ */
+@Data
+public class ResultModel<T> implements Serializable {
+
+    /**
+     * 序列号
+     */
+    private static final long serialVersionUID = -91231231434L;
+
+    /**
+     * 是否成功
+     */
+    private Boolean success;
+
+    /**
+     * 错误信息
+     */
+    private String msg;
+
+    /**
+     * 错误码
+     */
+    private String errorCode;
+
+    /**
+     * 数据
+     */
+    private T content;
+
+    /**
+     * @param data 结果数据
+     * @param <T>  结果类型
+     * @return 构造好的结果model
+     */
+    public static <T> ResultModel<T> success(T data) {
+        ResultModel<T> result = new ResultModel<>();
+        result.setSuccess(true);
+        result.setContent(data);
+        return result;
+    }
+
+    /**
+     * @param msg       错误信息
+     * @param errorCode 错误码
+     * @param <T>       结果类型
+     * @return 构造好的结果model
+     */
+    public static <T> ResultModel<T> failed(String msg, ErrorCode errorCode) {
+        ResultModel<T> result = new ResultModel<>();
+        result.setSuccess(false);
+        result.setErrorCode(errorCode.getCode());
+        result.setMsg(msg);
+        return result;
+    }
+
+    /**
+     * @param msg 错误信息
+     * @param <T> 结果类型
+     * @return 构造好的结果model
+     */
+    public static <T> ResultModel<T> failed(String msg) {
+        ResultModel<T> result = new ResultModel<>();
+        result.setSuccess(false);
+        result.setMsg(msg);
+        return result;
+    }
+
+}
