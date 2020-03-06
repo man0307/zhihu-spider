03eb56036adaf126b7c743d697e46db335e9f1ce
yehuo <1505191091@qq.com>
2020-03-06 17:27:37 +0800

    事件抽象类增加序列化接口

--- src/main/java/com/mcy/zhihuspider/eventframework/events/AbstractEvent.java
+++ src/main/java/com/mcy/zhihuspider/eventframework/events/AbstractEvent.java
@@ -3,14 +3,18 @@ package com.mcy.zhihuspider.eventframework.events;
 
 import com.mcy.zhihuspider.eventframework.Event;
 
+import java.io.Serializable;
+
 /**
  * @author chaoyang.man
  * @version 1.0
  * @date 2020/2/14 11:17 下午
  * <p>
- * 实现事件的抽象类
+ * 事件的抽象类
  */
-public abstract class AbstractEvent implements Event {
+public abstract class AbstractEvent implements Event, Serializable {
+
+    private static final long serialVersionUID = 123456789L;
 
     @Override
     public Class<? extends Event> getEventType() {
